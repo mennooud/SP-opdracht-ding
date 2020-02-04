9b1f5b1f7cbc545b2ce3c0b4e42cf30f4acd8477
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-04 14:02:10 +0100

    Opdracht 1 Pyramides

--- Sp opdracht 1 ofzo.py
+++ Sp opdracht 1 ofzo.py
@@ -6,10 +6,24 @@ for i in range(a):
     print("*"*(a-i-1))
 
 i = 1
-while i != a:
+while i != a+1:
     print("*"*i)
     i += 1
 i = 1
 while i != a:
     print("*"*(a-i))
+    i += 1
+
+for i in range(a):
+    print(" "*(a-i-1)+"*"*(i+1))
+for i in range(a):
+    print(" "*(i+1)+"*"*(a-i-1))
+
+i = 1
+while i != a+1:
+    print(" "*(a-i)+"*"*(i))
+    i += 1
+i = 1
+while i != a:
+    print(" "*(i)+"*"*(a-i))
     i += 1
