f4de583441e268745c56a26d3f7954e6dfa053de
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-04 14:26:48 +0100

    Opdracht 2 Tekst vergelijken

---
+++ Sp opdracht 2.py
@@ -0,0 +1,15 @@
+def Sp_Opdracht_2():
+    i = 0
+    c = 0
+    a = input("Geef tekst")
+    b = input("Geef meer tekst")
+    while i != len(a) and i != len(b):
+        if a[i] == b[i]:
+            c += 1
+            i += 1
+        else:
+            break
+    print("Je eerste index verschil zit op " + str(c))
+
+
+Sp_Opdracht_2()
