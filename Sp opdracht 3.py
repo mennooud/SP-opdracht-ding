bafe26621b244ca216eca26fcaf50604a92d653e
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-06 09:32:34 +0100

    Opdracht 3

--- Sp opdracht 3.py
+++ Sp opdracht 3.py
@@ -1,11 +1,42 @@
 
 lijstje = [1, 2, 6, 3, 34, 5, 1, 3, 5, 9]
-
-def count(lijstje):
-    a = input('Getal')
+a = input('Getal')
+def count(lijstje, a):
     b = 0
     for i in lijstje:
         if int(a) == i:
             b += 1
-    print("Dit getal komt " +b+" keer voor")
-count(lijstje)
+    return b
+
+
+print("Dit getal komt " + str(count(lijstje, a)) + " keer voor")
+
+
+
+def verschil(lijstje):
+    i = 0
+    m = 0
+    while i != (len(lijstje)-1):
+        n = lijstje[i] - lijstje[i+1]
+        if n < 0:
+            n = n*-1
+        if n > m:
+            m = n
+        i += 1
+    print(m)
+
+verschil(lijstje)
+
+lijst = [0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1]
+def functie(lijst):
+    x = count(lijst, 0)
+    if x > 12:
+        return(0)
+    else:
+        y = count(lijstje, 1)
+        if y < x:
+            return(1)
+        else:
+            return(2)
+
+print(functie(lijst))
