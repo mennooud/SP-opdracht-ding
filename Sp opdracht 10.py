c159eb5f5071fe92df8033d31d00da512e935775
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-11 13:10:15 +0100

    Iwan

--- Sp opdracht 10.py
+++ Sp opdracht 10.py
@@ -1,16 +1,13 @@
-a = int(input("Geef een Fibonaci getal: "))
-b = 0
-c = [0, 1]
+def vraag():
+    a = int(input("Geef een Fibonaci getal: "))
+    b = 0
+    c = [0, 1]
+    Fibonaci(a, b, c)
 def Fibonaci(FibonaciA, Rotatie, Lijst):
     if FibonaciA == Rotatie:
         print(Lijst[0])
     else:
-        Tel = Lijst[0] + Lijst[1]
-        Lijst[0] = Lijst[1]
-        Lijst[1] = Tel
+        Lijst[0], Lijst[1] = Lijst[1], Lijst[0] + Lijst[1]
         Rotatie += 1
         Fibonaci(FibonaciA, Rotatie, Lijst)
-
-
-
-Fibonaci(a, b, c)
+vraag()
