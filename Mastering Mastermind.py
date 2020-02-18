92bdab9b3b8c595d98d3678656a9ee40100ffc0e
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-18 12:44:00 +0100

    Mastermind Iwan

--- Mastering Mastermind.py
+++ Mastering Mastermind.py
@@ -1,5 +1,9 @@
 import random
 
+#Ik weet niet hoe ik verder kom met hetgeen wat ik al heb, als in ik weet niet hoe ik de beste keuze voor de pc nu moet laten uitrekenen. Ook weet ik niet hoe ik met de zwarte pins precies moet werken.
+#Heeft u hier een tip voor of iets waardoor ik weer verder kan?
+
+
 def start():
     wie = input("Wil je zelf gokken of de pc laten gokken?: ").lower()
     if wie == ("pc"):
@@ -9,7 +13,6 @@ def start():
             randomcode()
         else:
             start()
-
 
 def randomcode():
     code = []
@@ -29,7 +32,6 @@ def gok(code):
             print("Laatste kans")
         hint = []
         gok = []
-        gebruikt = []
         b = len(gok)
         zwartepin = 0
         wittepin = 0
@@ -78,11 +80,153 @@ def menscode():
             botcode.append(kleurdict[kleur])
         except KeyError:
             print("Deze kleur zit niet in de game")
-    print(botcode)
+    mogelijk = createans()
+    code = ''.join(map(str, botcode))
+    eenfunctienaam(mogelijk, code)
+
+def createans():
+    possibleans = []
+    for i in range(1111, 6667):
+        answer = str(i)
+        if '7' in answer or '8' in answer or '9' in answer or '0' in answer:
+            continue
+        else:
+            possibleans.append(answer)
+    print(possibleans)
+    return possibleans
+
+def eenfunctienaam(mogelijk, code):
+    lives = 10
+    attempts = 0
+
+    while lives > 0:
+        gok = mogelijk[0]
+        print(gok)
+        feedback = botstart(code, gok)
+
+        for i in reversed(mogelijk):
+            item_feedback = botstart(gok, i)
+
+            if feedback != item_feedback:
+                mogelijk.remove(i)
+
+        lives -= 1
+        attempts += 1
+
+        if code == gok:
+            print('gevonden')
+            print(attempts)
+            exit()
+        else:
+            if lives == 0:
+                print('game over')
 
 
+    #print(hint)
+    #gokstring = ''.join(map(str, gok))
+    #mogelijk.remove(gokstring)
+    #if sum(hint) == 4:
+    #    j = 0
+    #    nieuw = viergoed(mogelijk, gok, j)
+    #else:
+    #    if sum(hint) == 3:
+    #        j = 0
+    #        nieuw = driegoed(mogelijk, gok, j)
+    #    else:
+    #        if sum(hint) == 2:
+    #            j = 0
+    #            nieuw = tweegoed(mogelijk, gok, j)
+    #        else:
+    #            if sum(hint) == 1:
+    #                j = 0
+    #                nieuw = eengoed(mogelijk, gok, j)
+    #            else:
+    #                if sum(hint) == 0:
+    #                    j = 0
+    #                    nieuw = []
+    #                    nieuw = nulgoed(mogelijk, gok, j, nieuw)
+    #if hint != [0,0]:
+    #    j = 0
+    #    nieuw = verbeterdefeedback(nieuw, gok, j, hint)
+    #    j = 0
+    #    nieuw = wittepins(nieuw, gok, j, hint)
+    #return nieuw
 
+def wittepins(nieuw, gok, j, hint):
+    while j != len(nieuw):
+        woord = nieuw[j]
+        teller = 0
+        if str(gok[0]) in woord and str(gok[0]) != woord[0]:
+            teller += 1
+        if str(gok[1]) in woord and str(gok[1]) != woord[1]:
+            teller += 1
+        if str(gok[2]) in woord and str(gok[2]) != woord[2]:
+            teller += 1
+        if str(gok[3]) in woord and str(gok[3]) != woord[3]:
+            teller += 1
+        if teller == 4:
+            j += 1
+        else:
+            nieuw.remove(woord)
+            verbeterdefeedback(nieuw, gok, j, hint)
+    return nieuw
 
+def verbeterdefeedback(nieuw, gok, j, hint):
+    while j != len(nieuw):
+        woord = nieuw[j]
+        teller = 0
+        if str(gok[0]) == woord[0]:
+            teller += 1
+        if str(gok[1]) == woord[1]:
+            teller += 1
+        if str(gok[2]) == woord[2]:
+            teller += 1
+        if str(gok[3]) == woord[3]:
+            teller += 1
+        if teller == hint[0]:
+            j += 1
+        else:
+            nieuw.remove(woord)
+            verbeterdefeedback(nieuw, gok, j, hint)
+    return nieuw
 
+def botstart(strcode, gok):
+    code = []
+    for item in strcode:
+        code.append(int(item))
+    nieuw = [0, 0, 0, 0]
+    hint = [0, 0]
+    lengtehint = 0
+    aanpascode = [0, 0, 0, 0]
+    while lengtehint != 4:
+        if code[lengtehint] == int(gok[lengtehint]):
+            nieuw[lengtehint] = 1
+            aanpascode[lengtehint] = 99
+            hint[0] = hint[0] + 1
+        else:
+            aanpascode[lengtehint] = code[lengtehint]
+        lengtehint += 1
+    lengtehint = 0
+    while lengtehint != 4:
+        if int(gok[lengtehint]) in aanpascode:
+            if nieuw[lengtehint] != 1:
+                hint[1] += 1
+                aanpascode[int(gok[lengtehint])-1] = 9
+        lengtehint += 1
+    #print(hint)
+        #if gok[lengtehint] in code:
+        #    if aanpascode[lengtehint] != 99:
+        #        aanpas = int(aanpascode.index(nieuw[lengtehint]))
+        #        print(aanpas)
+#
+        #        aanpascode[aanpas] = 99
+        #        print(hint, gok[lengtehint])
+        #        hint[1] = hint[1] + 1
+#       #        else:
+#       #            aanpas = int(aanpascode.index(nieuw[lengtehint]))
+#       #            aanpascode[aanpas] = 'gebruikt'
+#       #            hint[1] = hint[1] + 1
+        #lengtehint += 1
+    return hint
 
 start()
